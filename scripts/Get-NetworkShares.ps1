7ca5f351077e250063e5f0d1a0426ec2690bff2a
Christian Zagazeta <46060355+yourlastnamesoundslikeatypeofpasta@users.noreply.github.com>
2023-12-28 18:52:35 -0800

    Initial Commit

---
+++ scripts/Get-NetworkShares.ps1
@@ -0,0 +1,53 @@
+
+function Get-NetworkShares {
+    <#
+    .SYNOPSIS
+        Get network shares from a computer
+    .DESCRIPTION
+        Get network shares from a computer
+    .PARAMETER ComputerName
+        The name of the computer to get network shares from
+    .EXAMPLE
+        Get-NetworkShares -ComputerName "$ENV:COMPUTERNAME"    
+    .NOTES
+    #>
+
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]$ComputerName
+    )
+
+    # if $ComputerName is null, use the local computer
+    if ($ComputerName -eq $null) {
+        $ComputerName = $env:COMPUTERNAME
+    }
+
+    $shares = Get-CimInstance -ClassName Win32_Share -ComputerName $ComputerName
+
+    $result = [pscustomobject]@{
+        ComputerName = $env:COMPUTERNAME
+        Shares = @(
+        foreach ($share in $shares) {
+        [pscustomobject]@{
+        ShareName = $share.Name
+        Path = $share.Path
+        Description = $share.Description
+        Type = $share.Type
+                }
+            }
+        )
+    }
+
+    return $result
+}
+
+# save network shares to a file
+$result = Get-NetworkShares -ComputerName $env:COMPUTERNAME
+
+# export $result to xml file
+$result | Export-Clixml -Path "$($env:COMPUTERNAME)-NetworkShares"
+
+
+
+
