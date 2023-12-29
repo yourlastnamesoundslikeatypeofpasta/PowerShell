165b6ae936fb980e3eae9351a8d3845a51903644
Christian Zagazeta <46060355+yourlastnamesoundslikeatypeofpasta@users.noreply.github.com>
2023-12-29 13:18:28 -0800

    Initial Commit

---
+++ scripts/Set-TimeZoneEasternStandardTime.ps1
@@ -0,0 +1,18 @@
+function Set-TimeZoneEasternStandardTime {
+    <#
+    .SYNOPSIS
+    Set time zone to Eastern Standard Time.
+    
+    .DESCRIPTION
+    Set time zone to Eastern Standard Time using the Set-TimeZone cmdlet.
+    
+    .EXAMPLE
+    Set-TimeZoneEasternStandardTime
+    
+    .NOTES
+    Can be used as a one liner.
+    #>
+    Set-TimeZone -ID "Eastern Standard Time"
+}
+
+Set-TimeZoneEasternStandardTime
