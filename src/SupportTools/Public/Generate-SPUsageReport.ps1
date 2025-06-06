f4c029da6950331a59ee742d64d8c9ea8b6bfd85
Christian Zagazeta <46060355+yourlastnamesoundslikeatypeofpasta@users.noreply.github.com>
2025-06-06 19:34:46 -0400

    Add SharePoint usage report automation

---
+++ src/SupportTools/Public/Generate-SPUsageReport.ps1
@@ -0,0 +1,17 @@
+function Generate-SPUsageReport {
+    <#
+    .SYNOPSIS
+        Generate library usage CSV reports and create Service Desk tickets when limits are exceeded.
+    .DESCRIPTION
+        This is a wrapper for the Generate-SPUsageReport.ps1 script in the scripts folder.
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromRemainingArguments=$true, ValueFromPipeline=$true)]
+        [object[]]$Arguments,
+        [string]$TranscriptPath
+    )
+    process {
+        Invoke-ScriptFile -Name 'Generate-SPUsageReport.ps1' -Args $Arguments -TranscriptPath $TranscriptPath
+    }
+}
