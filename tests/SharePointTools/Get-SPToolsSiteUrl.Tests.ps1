fea242362c56459091c22dd8d2821f6d205f235a
Christian Zagazeta <46060355+yourlastnamesoundslikeatypeofpasta@users.noreply.github.com>
2025-06-07 15:56:49 -0400

    Fix Pester module scope setup

---
+++ tests/SharePointTools/Get-SPToolsSiteUrl.Tests.ps1
@@ -0,0 +1,41 @@
+Describe 'Get-SPToolsSiteUrl function' {
+    BeforeAll {
+        Import-Module $PSScriptRoot/../../src/SharePointTools/SharePointTools.psd1 -Force
+    }
+
+    BeforeEach {}
+
+    It 'returns the matching URL' {
+        InModuleScope SharePointTools {
+            $ExecutionContext.SessionState.PSVariable.Set('SharePointToolsSettings', @{ Sites = @{ A='https://a'; B='https://b' } })
+            Mock Write-SPToolsHacker {}
+            Get-SPToolsSiteUrl -SiteName 'A' | Should -Be 'https://a'
+        }
+    }
+
+    It 'supports pipeline input' {
+        InModuleScope SharePointTools {
+            $ExecutionContext.SessionState.PSVariable.Set('SharePointToolsSettings', @{ Sites = @{ A='https://a'; B='https://b' } })
+            Mock Write-SPToolsHacker {}
+            [pscustomobject]@{ SiteName='B' } | Get-SPToolsSiteUrl | Should -Be 'https://b'
+        }
+    }
+
+    It 'throws when site is missing' {
+        InModuleScope SharePointTools {
+            $ExecutionContext.SessionState.PSVariable.Set('SharePointToolsSettings', @{ Sites = @{ A='https://a' } })
+            Mock Write-SPToolsHacker {}
+            { Get-SPToolsSiteUrl -SiteName 'C' } | Should -Throw
+        }
+    }
+
+    It 'logs lookup messages' {
+        InModuleScope SharePointTools {
+            $ExecutionContext.SessionState.PSVariable.Set('SharePointToolsSettings', @{ Sites = @{ A='https://a' } })
+            Mock Write-SPToolsHacker {}
+            Get-SPToolsSiteUrl -SiteName 'A' | Out-Null
+            Assert-MockCalled Write-SPToolsHacker -Times 1 -ParameterFilter { $Message -eq 'Looking up A' }
+            Assert-MockCalled Write-SPToolsHacker -Times 1 -ParameterFilter { $Message -eq 'URL found: https://a' }
+        }
+    }
+}
---
+++ tests/SupportTools/Add-UserToGroup.Tests.ps1
@@ -0,0 +1,35 @@
+Describe 'Add-UserToGroup function' {
+    BeforeAll {
+        Import-Module $PSScriptRoot/../../src/SupportTools/SupportTools.psd1 -Force
+    }
+
+    It 'passes parameters to Invoke-ScriptFile' {
+        InModuleScope SupportTools {
+            Mock Invoke-ScriptFile {} -ModuleName SupportTools
+            Add-UserToGroup -CsvPath 'users.csv' -GroupName 'TeamA'
+            Assert-MockCalled Invoke-ScriptFile -ModuleName SupportTools -Times 1 -ParameterFilter {
+                $Name -eq 'AddUsersToGroup.ps1'
+            }
+        }
+    }
+
+    It 'accepts pipeline input' {
+        InModuleScope SupportTools {
+            Mock Invoke-ScriptFile {} -ModuleName SupportTools
+            [pscustomobject]@{ CsvPath='input.csv'; GroupName='G1' } | Add-UserToGroup
+            Assert-MockCalled Invoke-ScriptFile -ModuleName SupportTools -Times 1 -ParameterFilter {
+                $Name -eq 'AddUsersToGroup.ps1'
+            }
+        }
+    }
+
+    It 'forwards transcript and switches' {
+        InModuleScope SupportTools {
+            Mock Invoke-ScriptFile {} -ModuleName SupportTools
+            Add-UserToGroup -TranscriptPath 't.log' -Simulate -Explain
+            Assert-MockCalled Invoke-ScriptFile -ModuleName SupportTools -Times 1 -ParameterFilter {
+                $TranscriptPath -eq 't.log' -and $Simulate -and $Explain
+            }
+        }
+    }
+}
