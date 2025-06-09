fb388039351b7e48774d9cbc61895d3d06b3f6cb
Christian Zagazeta <46060355+yourlastnamesoundslikeatypeofpasta@users.noreply.github.com>
2025-06-08 20:41:55 -0400

    feat: load ServiceDesk token from secret

--- src/ServiceDeskTools/Private/Invoke-SDRequest.ps1
+++ src/ServiceDeskTools/Private/Invoke-SDRequest.ps1
@@ -5,15 +5,22 @@ function Invoke-SDRequest {
         [Parameter(Mandatory)][string]$Path,
         [hashtable]$Body,
         [switch]$ChaosMode,
-        [string]$BaseUri
+        [string]$BaseUri,
+        [string]$Vault
     )
     Assert-ParameterNotNull $Method 'Method'
     Assert-ParameterNotNull $Path 'Path'
 
     $baseUri = if ($BaseUri) { $BaseUri } else { $env:SD_BASE_URI }
     if (-not $baseUri) { $baseUri = 'https://api.samanage.com' }
+
     $token = $env:SD_API_TOKEN
-    if (-not $token) { throw 'SD_API_TOKEN environment variable must be set.' }
+    if (-not $token) {
+        $getParams = @{ Name = 'SD_API_TOKEN'; AsPlainText = $true; ErrorAction = 'SilentlyContinue' }
+        if ($PSBoundParameters.ContainsKey('Vault')) { $getParams.Vault = $Vault }
+        $token = Get-Secret @getParams
+        if ($token) { $env:SD_API_TOKEN = $token } else { throw 'SD_API_TOKEN environment variable must be set.' }
+    }
 
     if (-not $ChaosMode) { $ChaosMode = [bool]$env:ST_CHAOS_MODE }
     if ($ChaosMode) {
