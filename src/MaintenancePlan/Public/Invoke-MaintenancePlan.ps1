435b07586080ee32185bddc0b7b4cfe57b856b0b
Christian Zagazeta <46060355+yourlastnamesoundslikeatypeofpasta@users.noreply.github.com>
2025-06-09 17:26:16 -0400

    Remove custom WhatIf param

--- src/MaintenancePlan/Public/Invoke-MaintenancePlan.ps1
+++ src/MaintenancePlan/Public/Invoke-MaintenancePlan.ps1
@@ -4,20 +4,17 @@ function Invoke-MaintenancePlan {
         Execute all steps in a maintenance plan.
     .PARAMETER Plan
         Plan object created by New-MaintenancePlan or Import-MaintenancePlan.
-    .PARAMETER WhatIf
-        Display commands without executing.
     #>
     [CmdletBinding(SupportsShouldProcess=$true)]
     param(
         [Parameter(Mandatory)]
         [ValidateNotNull()]
-        [object]$Plan,
-        [switch]$WhatIf
+        [object]$Plan
     )
     Assert-ParameterNotNull $Plan 'Plan'
     foreach ($step in $Plan.Steps) {
         Write-STStatus "Running $step" -Level INFO -Log
-        if (-not $WhatIf) {
+        if ($PSCmdlet.ShouldProcess($step)) {
             if (Test-Path $step) {
                 & $step
             } else {
