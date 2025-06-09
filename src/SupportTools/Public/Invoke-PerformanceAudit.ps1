58644b229de89692f6642a47504ece44b2884df4
Christian Zagazeta <46060355+yourlastnamesoundslikeatypeofpasta@users.noreply.github.com>
2025-06-09 17:20:12 -0400

    style: flatten Invoke-PerformanceAudit parameters

--- src/SupportTools/Public/Invoke-PerformanceAudit.ps1
+++ src/SupportTools/Public/Invoke-PerformanceAudit.ps1
@@ -38,21 +38,13 @@ function Invoke-PerformanceAudit {
     #>
     [CmdletBinding(SupportsShouldProcess=$true)]
     param(
-        [Parameter(Mandatory = $false, ValueFromRemainingArguments = $true, ValueFromPipeline = $true)]
-        [object[]]$Arguments,
-        [Parameter(Mandatory = $false)]
-        [ValidateNotNullOrEmpty()]
-        [string]$TranscriptPath,
-        [Parameter(Mandatory = $false)]
-        [switch]$Simulate,
-        [Parameter(Mandatory = $false)]
-        [switch]$Explain
-        ,[Parameter(Mandatory = $false)]
-        [object]$Logger
-        ,[Parameter(Mandatory = $false)]
-        [object]$TelemetryClient
-        ,[Parameter(Mandatory = $false)]
-        [object]$Config
+        [Parameter(Mandatory = $false, ValueFromRemainingArguments = $true, ValueFromPipeline = $true)][object[]]$Arguments,
+        [Parameter(Mandatory = $false)] [ValidateNotNullOrEmpty()] [string]$TranscriptPath,
+        [Parameter(Mandatory = $false)][switch]$Simulate,
+        [Parameter(Mandatory = $false)][switch]$Explain,
+        [Parameter(Mandatory = $false)][object]$Logger,
+        [Parameter(Mandatory = $false)][object]$TelemetryClient,
+        [Parameter(Mandatory = $false)][object]$Config
     )
     process {
         try {
