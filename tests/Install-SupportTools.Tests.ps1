2cf46cfdce7aeaeb7a7c13b63c5e32ec064e73b8
Christian Zagazeta <46060355+yourlastnamesoundslikeatypeofpasta@users.noreply.github.com>
2025-06-09 10:39:12 -0400

    Improve Install-SupportTools status output

--- tests/Install-SupportTools.Tests.ps1
+++ tests/Install-SupportTools.Tests.ps1
@@ -4,13 +4,13 @@ Describe 'Install-SupportTools script' {
     BeforeEach {
         function Import-Module {}
         Mock Import-Module {}
+        Mock Write-STStatus {}
     }
 
     Safe-It 'imports from src' {
-        $warnings = @()
-        & $PSScriptRoot/../scripts/Install-SupportTools.ps1 -WarningVariable +warnings -Scope CurrentUser
+        & $PSScriptRoot/../scripts/Install-SupportTools.ps1 -Scope CurrentUser
         $spPath = Join-Path $PSScriptRoot/../src/SharePointTools 'SharePointTools.psd1'
         Assert-MockCalled Import-Module -ParameterFilter { $Name -eq $spPath } -Times 1
-        $warnings | Should -Contain "Imported SharePointTools from $spPath"
+        Assert-MockCalled Write-STStatus -ParameterFilter { $Message -eq "Imported SharePointTools from $spPath" -and $Level -eq 'SUCCESS' } -Times 1
     }
 }
