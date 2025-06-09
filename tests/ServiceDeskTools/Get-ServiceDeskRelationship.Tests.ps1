92aceae8483ff968a0528983e7bb3683b6635bb7
Christian Zagazeta <46060355+yourlastnamesoundslikeatypeofpasta@users.noreply.github.com>
2025-06-08 21:20:00 -0400

    Add Get-ServiceDeskRelationship command

---
+++ tests/ServiceDeskTools/Get-ServiceDeskRelationship.Tests.ps1
@@ -0,0 +1,35 @@
+. $PSScriptRoot/../TestHelpers.ps1
+
+Describe 'Get-ServiceDeskRelationship' {
+    BeforeAll {
+        Import-Module $PSScriptRoot/../../src/Logging/Logging.psd1 -Force
+        Import-Module $PSScriptRoot/../../src/ServiceDeskTools/ServiceDeskTools.psd1 -Force
+    }
+
+    Safe-It 'filters by asset id and type' {
+        InModuleScope ServiceDeskTools {
+            Mock Invoke-SDRequest { @{ ok = $true } }
+            $res = Get-ServiceDeskRelationship -AssetId 5 -Type 'parent'
+            Assert-MockCalled Invoke-SDRequest -Times 1 -ParameterFilter {
+                $Method -eq 'GET' -and
+                $Path -eq '/asset_relationships.json?asset_id=5&relationship_type=parent'
+            }
+            $res.ok | Should -Be $true
+        }
+    }
+
+    Safe-It 'passes ChaosMode' {
+        InModuleScope ServiceDeskTools {
+            Mock Invoke-SDRequest { @{ ok = $true } }
+            Get-ServiceDeskRelationship -ChaosMode
+            Assert-MockCalled Invoke-SDRequest -Times 1 -ParameterFilter { $ChaosMode -eq $true }
+        }
+    }
+
+    Safe-It 'throws when Invoke-SDRequest fails' {
+        InModuleScope ServiceDeskTools {
+            Mock Invoke-SDRequest { throw 'bad' }
+            { Get-ServiceDeskRelationship -AssetId 1 } | Should -Throw
+        }
+    }
+}
