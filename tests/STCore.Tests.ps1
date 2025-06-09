9a378037152e5a694a85fa29d49e570aac06b8c5
Christian Zagazeta <46060355+yourlastnamesoundslikeatypeofpasta@users.noreply.github.com>
2025-06-09 17:29:01 -0400

    test: verify STCore exports

--- tests/STCore.Tests.ps1
+++ tests/STCore.Tests.ps1
@@ -5,7 +5,30 @@ Describe 'STCore Module' {
         Import-Module $PSScriptRoot/../src/STCore/STCore.psd1 -Force
     }
 
-    Safe-It 'exports Invoke-STRequest' {
-        (Get-Command -Module STCore).Name | Should -Contain 'Invoke-STRequest'
+    Context 'Exported commands' {
+        $expected = @(
+            'Assert-ParameterNotNull',
+            'New-STErrorObject',
+            'New-STErrorRecord',
+            'Write-STDebug',
+            'Test-IsElevated',
+            'Get-STConfig',
+            'Get-STConfigValue',
+            'Invoke-STRequest'
+        )
+
+        $exported = (Get-Command -Module STCore).Name
+        foreach ($cmd in $expected) {
+            Safe-It "exports $cmd" {
+                $exported | Should -Contain $cmd
+            }
+        }
+    }
+
+    Safe-It 'Show-STCoreBanner reports module version' {
+        $banner = Show-STCoreBanner
+        $manifest = Import-PowerShellDataFile $PSScriptRoot/../src/STCore/STCore.psd1
+        $banner.Module  | Should -Be 'STCore'
+        $banner.Version | Should -Be $manifest.ModuleVersion
     }
 }
